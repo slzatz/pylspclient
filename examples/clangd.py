221965a3cb9dc3867c5913cdc695f4abc2e36f76
slzatz <slzatz@gmail.com>
2020-10-11 08:55:37 -0400

    clangd.py and added client_log and test.cpp: beginning to analyze pylspclient

--- examples/clangd.py
+++ examples/clangd.py
@@ -16,7 +16,7 @@ class ReadPipe(threading.Thread):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='pylspclient example with clangd')
-    parser.add_argument('clangd_path', type=str, default="/usr/bin/clangd-6.0", 
+    parser.add_argument('clangd_path', type=str, default="/usr/bin/clangd", 
                     help='the clangd path', nargs="?")
     args = parser.parse_args()
     p = subprocess.Popen([args.clangd_path], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -25,8 +25,9 @@ if __name__ == "__main__":
     json_rpc_endpoint = pylspclient.JsonRpcEndpoint(p.stdin, p.stdout)
     # To work with socket: sock_fd = sock.makefile()
     lsp_endpoint = pylspclient.LspEndpoint(json_rpc_endpoint)
-
     lsp_client = pylspclient.LspClient(lsp_endpoint)
+
+
     capabilities = {'textDocument': {'codeAction': {'dynamicRegistration': True},
     'codeLens': {'dynamicRegistration': True},
     'colorProvider': {'dynamicRegistration': True},
@@ -139,18 +140,25 @@ if __name__ == "__main__":
         25,
         26]}},'workspaceEdit': {'documentChanges': True},
     'workspaceFolders': True}}
-    root_uri = 'file:///home/osboxes/projects/ctest/'
+
+    root_uri = 'file:///home/slzatz/pylspclient/examples/'
+
     workspace_folders = [{'name': 'python-lsp', 'uri': root_uri}]
+
+    print("\n### client sends initialize:\n")
     print(lsp_client.initialize(p.pid, None, root_uri, None, capabilities, "off", workspace_folders))
+    print("\n### client sends initialized:\n")
     print(lsp_client.initialized())
 
-    file_path = "/home/osboxes/projects/ctest/test.c"
+    file_path = "/home/slzatz/pylspclient/examples/test.cpp"
     uri = "file://" + file_path
     text = open(file_path, "r").read()
-    languageId = pylspclient.lsp_structs.LANGUAGE_IDENTIFIER.C
+    languageId = pylspclient.lsp_structs.LANGUAGE_IDENTIFIER.CPP
     version = 1
+    print("\n### client sends didOpen:\n")
     lsp_client.didOpen(pylspclient.lsp_structs.TextDocumentItem(uri, languageId, version, text))
     try:
+        print("\n### client sends documentSymbol:\n")
         symbols = lsp_client.documentSymbol(pylspclient.lsp_structs.TextDocumentIdentifier(uri))
         for symbol in symbols:
             print(symbol.name)
@@ -158,9 +166,9 @@ if __name__ == "__main__":
         # documentSymbol is supported from version 8.
         print("Failed to document symbols")
 
-    lsp_client.definition(pylspclient.lsp_structs.TextDocumentIdentifier(uri), pylspclient.lsp_structs.Position(14, 4))
-    lsp_client.signatureHelp(pylspclient.lsp_structs.TextDocumentIdentifier(uri), pylspclient.lsp_structs.Position(14, 4))
-    lsp_client.definition(pylspclient.lsp_structs.TextDocumentIdentifier(uri), pylspclient.lsp_structs.Position(14, 4))
-    lsp_client.completion(pylspclient.lsp_structs.TextDocumentIdentifier(uri), pylspclient.lsp_structs.Position(14, 4), pylspclient.lsp_structs.CompletionContext(pylspclient.lsp_structs.CompletionTriggerKind.Invoked))
+    #lsp_client.definition(pylspclient.lsp_structs.TextDocumentIdentifier(uri), pylspclient.lsp_structs.Position(14, 4))
+    #lsp_client.signatureHelp(pylspclient.lsp_structs.TextDocumentIdentifier(uri), pylspclient.lsp_structs.Position(14, 4))
+    #lsp_client.definition(pylspclient.lsp_structs.TextDocumentIdentifier(uri), pylspclient.lsp_structs.Position(14, 4))
+    #lsp_client.completion(pylspclient.lsp_structs.TextDocumentIdentifier(uri), pylspclient.lsp_structs.Position(14, 4), pylspclient.lsp_structs.CompletionContext(pylspclient.lsp_structs.CompletionTriggerKind.Invoked))
     lsp_client.shutdown()
     lsp_client.exit()
---
+++ examples/test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main() {
+std::vector<int> v{1,2,3,4};
+std::string s{"Hello World"}
+
+std::cout << s << std::endl;
+std::cout << v.at(2) << std::endl;
+return 0;
+
+}
