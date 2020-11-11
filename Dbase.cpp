1e0fc8709f66b3cf715e6dd716514d1995578020
Steve Zatz <slzatz@gmail.com>
2020-11-11 10:50:20 -0500

    Dbase.h, Dbase.cpp: cleaned out all unused code with Sqlite connecting to db (and destructor should close connection) and Query class doing most of the work:

--- Dbase.cpp
+++ Dbase.cpp
@@ -1,9 +1,6 @@
 #include "Dbase.h"
 #include <iostream>
 
-//typedef int (*sq_callback)(void *, int, char **, char **); //sqlite callback type
-//using sq_callback = int (*)(void *, int, char **, char **);
-
 Sqlite::Sqlite(std::string db_path) {
   int rc = sqlite3_open(db_path.c_str(), &db);
   if (rc != SQLITE_OK) {
@@ -11,49 +8,6 @@ Sqlite::Sqlite(std::string db_path) {
     exit(1);
   }
 }
-
-/*
-bool Sqlite::run() {
-   int rc = sqlite3_exec(db, sql.c_str(), callback, pArg, errmsg);
-   if (rc != SQLITE_OK) {
-     //outlineShowMessage("SQL error in %s: %s", func, errmsg);
-     sqlite3_free(errmsg);
-     return false;
-   }
-   pArg = nullptr;
-   callback = nullptr;
-   return true;
-}
-
-void Sqlite::params(sq_callback cb, void * arg) {
-  callback = cb;
-  pArg = arg;
-}
-
-int Sqlite::prepare(void) {
-  return sqlite3_prepare_v2(db, sql.c_str(), -1, &res, 0);
-}
-
-int Sqlite::step(void) {
-  return sqlite3_step(res);
- }
-
-void Sqlite::finalize(void) {
-  sqlite3_finalize(res);
-}
-
-std::string Sqlite::column_text(int col) {
-  return std::string(reinterpret_cast<const char *>(sqlite3_column_text(res, col)));
-}
-
-int Sqlite::column_int(int col) {
-  return sqlite3_column_int(res, col);
-}
-
-bool Sqlite::column_bool(int col) {
-  return (sqlite3_column_int(res, col) == 1) ? true : false;
-}
-*/
 
 int Query::step(void) {
   return sqlite3_step(res);
@@ -69,24 +23,4 @@ int Query::column_int(int col) {
 bool Query::column_bool(int col) {
   return (sqlite3_column_int(res, col) == 1) ? true : false;
 }
-/*
-std::string Sqlite::column_text(sqlite3_stmt *r, int col) {
-  return std::string(reinterpret_cast<const char *>(sqlite3_column_text(r, col)));
-}
 
-int Sqlite::column_int(sqlite3_stmt *r, int col) {
-  return sqlite3_column_int(r, col);
-}
-
-bool Sqlite::column_bool(sqlite3_stmt *r, int col) {
-  return (sqlite3_column_int(r, col) == 1) ? true : false;
-}
-*/
-/*
-Sqlite lm_db(LM_DB);
-Sqlite fts_db(FTS_DB);
-
-void do_something() {
-  lm_db.query("fjalkdsf {} lkdsjflskdjflds {} lksdjfldsj {}", x, y, z);
-  lm_db.run_query()
-*/  
--- Dbase.h
+++ Dbase.h
@@ -1,43 +1,16 @@
 #include <sqlite3.h>
 #include <string>
 #include <fmt/core.h>
-#include <iostream>
+//#include <iostream>
 
 //typedef int (*sq_callback)(void *, int, char **, char **); //sqlite callback type
 using sq_callback = int (*)(void *, int, char **, char **);
 
 class Sqlite {
   public:
-    Sqlite(std:: string db_path);
-
-    template<typename... Args>
-    void query(fmt::string_view format_str, const Args & ... args) {
-      fmt::format_args argspack = fmt::make_format_args(args...);
-      sql = fmt::vformat(format_str, argspack);
-    }
-
-    template<typename... Args>
-    int query2(fmt::string_view format_str, const Args & ... args) {
-      fmt::format_args argspack = fmt::make_format_args(args...);
-      sql = fmt::vformat(format_str, argspack);
-      return sqlite3_prepare_v2(db, sql.c_str(), -1, &res, 0);
-    }
-
-    bool run(void);
-    int prepare(void);
-    void params(sq_callback, void *);
-    int step(void);
-    std::string column_text(int);
-    int column_int(int);
-    bool column_bool(int);
-    void finalize(void);
+    Sqlite(std::string db_path);
 
     sqlite3 *db;
-    sq_callback callback;
-    void *pArg;
-    char **errmsg;
-    std::string sql;
-    sqlite3_stmt *res;
 };
 
 class Query {
@@ -48,24 +21,20 @@ class Query {
     Query(Sqlite &db, fmt::string_view format_str, const Args & ... args) {
       fmt::format_args argspack = fmt::make_format_args(args...);
       sql = fmt::vformat(format_str, argspack);
-      d = &db;
-      //result = sqlite3_prepare_v2(db.db, sql.c_str(), -1, &db.res, 0);
       result = sqlite3_prepare_v2(db.db, sql.c_str(), -1, &res, 0);
     }
 
     ~Query() {
-     std::cout << "About to finalize" << std::endl; 
-     //sqlite3_finalize(d->res);
      sqlite3_finalize(res);
-     std::cout << "Finalized" << std::endl; 
+     fmt::print("Finalized {}\n", sql);
     }
 
     int step(void);
     std::string column_text(int);
     int column_int(int);
     bool column_bool(int);
+
     std::string sql;
-    Sqlite * d;
     int result;
     sqlite3_stmt *res;
 };
